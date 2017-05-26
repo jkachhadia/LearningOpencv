4c2ddbfb6fbf93b23a8e71531e04bda90cb9f38d
jkachhadia <jaykachhadia@hotmail.com>
2017-05-26 19:38:55 +0530

    added the template matcher - advanced version :D

--- template_match/tempmatch.py
+++ template_match/tempmatch.py
@@ -1,21 +1,31 @@
 import cv2
 import numpy as np
+select=False
+def mouse_callback(event,x,y,flags,params):
+    global select,frame,template,w,h,gray
+    if event == cv2.EVENT_LBUTTONDOWN:
+        select=False
+        template=gray[x:(x+40),y:(y+40)]
+        w,h=template.shape[::-1]
+        print template.shape
+        select=True
+global gray
+cv2.namedWindow('detected')
+cv2.setMouseCallback('detected',mouse_callback)
 
 cap=cv2.VideoCapture(0)
-template=cv2.imread("e.jpg",0)
-w,h=template.shape[::-1]
 while True:
     _,frame=cap.read()
     gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    if select==True:
+        res=cv2.matchTemplate(gray,template,cv2.TM_CCOEFF_NORMED)
+        threshold=0.95
+        loc=np.where(res>=threshold)
 
-    res=cv2.matchTemplate(gray,template,cv2.TM_CCOEFF_NORMED)
-    threshold=0.60
-    loc=np.where(res>=threshold)
+        for pt in zip(*loc[::-1]):
+            cv2.rectangle(frame,pt,(pt[0]+w,pt[1]+h),(0,255,255),2)
 
-    for pt in zip(*loc[::-1]):
-        cv2.rectangle(frame,pt,(pt[0]+w,pt[1]+h),(0,255,255),2)
-
-    cv2.imshow("detected",frame)
+    cv2.imshow('detected',frame)
 
 
 
