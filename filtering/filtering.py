fec8ee500db3ffd3d057cb56497a5f2d17e82036
jkachhadia <jaykachhadia@hotmail.com>
2017-05-26 10:44:16 +0530

    added smoothing and blurring techniques

--- filtering/filtering.py
+++ filtering/filtering.py
@@ -11,6 +11,17 @@ while True:
 
     mask=cv2.inRange(hsv,lower_red,upper_red)
     res=cv2.bitwise_and(frame,frame,mask=mask)
+
+    kernel=np.ones((15,15),np.float32)/225
+    smoothed=cv2.filter2D(res,-1,kernel)
+    blur=cv2.GaussianBlur(res,(15,15),0)
+    median=cv2.medianBlur(res,15)
+    bilateral=cv2.bilateralFilter(res,15,75,75)
+
+    cv2.imshow("bilateral",bilateral)
+    cv2.imshow("median",median)
+    cv2.imshow("blur",blur)
+    cv2.imshow("smoothed",smoothed)
     cv2.imshow("original",frame)
     cv2.imshow("mask",mask)
     cv2.imshow("res",res)
